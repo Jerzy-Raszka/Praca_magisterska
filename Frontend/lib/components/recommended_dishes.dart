db443052c56c354b0d8015305abf492737c8cc89
Jerzy Raszka <jerzyraszka2000@gmail.com>
2025-02-17 11:32:32 +0100

    feat: added recommended dishes page

---
+++ Frontend/lib/components/recommended_dishes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RecommendedDishes extends StatelessWidget {
+  const RecommendedDishes({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Text('test');
+  }
+}
