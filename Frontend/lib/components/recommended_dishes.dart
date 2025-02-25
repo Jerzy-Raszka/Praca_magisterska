6d65a831038c275a3bba71ccca2b6f51273365f5
Jerzy Raszka <jerzyraszka2000@gmail.com>
2025-02-25 15:01:04 +0100

    feat: base for reusable appbar and drawer

--- Frontend/lib/components/recommended_dishes.dart
+++ Frontend/lib/components/recommended_dishes.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RecommendedDishes extends StatelessWidget {
   const RecommendedDishes({super.key});
   @override
   Widget build(BuildContext context) {
-    return Text('test');
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 245, 228),
+      drawer: Drawer(), //TODO: universal drawer widget
+      appBar: appBar(),
+      body: Text('kappa'), //TODO: body: widget builder
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Polecane przepisy',
+        style: GoogleFonts.roboto(
+          textStyle: TextStyle(color: Color.fromARGB(255, 255, 245, 228)),
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 149, 35, 35),
+      elevation: 0.0,
+      centerTitle: true,
+    );
   }
 }
