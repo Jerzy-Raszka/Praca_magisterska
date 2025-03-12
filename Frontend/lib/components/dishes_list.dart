adf76aa29b1dce3c25b908a9d8b74b366bba1b4c
Jerzy Raszka <jerzyraszka2000@gmail.com>
2025-03-12 12:56:14 +0100

    feat: widget builder for dishes

--- Frontend/lib/components/dishes_list.dart
+++ Frontend/lib/components/dishes_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/dish.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -34,13 +35,89 @@ class _DishesList extends State<DishesList> {
     }
   }
 
-  //TODO: widget factory that build widget for dishes
+  //TODO: OnTap open overlay with specific data
+  //TODO: depending on argument shows delete or subscribe and opens diffrent overlays
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: dishesData.length,
       itemBuilder: (BuildContext context, int index) {
-        return Card(child: ListTile(title: Text(dishesData[index].name)));
+        return Center(
+          child: Container(
+            margin: EdgeInsets.all(5),
+            width: MediaQuery.sizeOf(context).width * 0.9,
+            height: 230,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 3,
+                color: Color.fromARGB(255, 149, 35, 35),
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              color: Color.fromARGB(255, 149, 35, 35),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(5),
+                  height: 170,
+                  width: MediaQuery.sizeOf(context).width * 0.8,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(dishesData[index].pictureUrl),
+                      fit: BoxFit.fill,
+                    ),
+                    border: Border.all(
+                      width: 3,
+                      color: const Color.fromARGB(255, 255, 245, 228),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: const Color.fromARGB(255, 255, 245, 228),
+                  ),
+                ),
+                Stack(
+                  children: [
+                    Center(
+                      child: Container(
+                        constraints: BoxConstraints(
+                          maxWidth: MediaQuery.sizeOf(context).width * 0.7,
+                        ),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 3,
+                            color: const Color.fromARGB(255, 255, 245, 228),
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          color: const Color.fromARGB(255, 255, 245, 228),
+                        ),
+                        child: Text(
+                          dishesData[index].name,
+                          style: GoogleFonts.roboto(
+                            textStyle: TextStyle(
+                              color: Color.fromARGB(255, 149, 35, 35),
+                            ),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: Icon(
+                        Icons.delete,
+                        size: 35,
+                        color: const Color.fromARGB(255, 255, 245, 228),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
       },
     );
   }
