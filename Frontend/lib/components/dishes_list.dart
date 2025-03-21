6d1f16d2467714675067aebc2e545e9ce164af13
Jerzy Raszka <jerzyraszka2000@gmail.com>
2025-03-21 12:01:32 +0100

    feat: added dialog design

--- Frontend/lib/components/dishes_list.dart
+++ Frontend/lib/components/dishes_list.dart
@@ -23,7 +23,7 @@ class _DishesList extends State<DishesList> {
   //192.168.0.10 -Gli || 192.168.3.4 -Cis
   void _fetchData() async {
     final response = await http.get(
-      Uri.parse('http://192.168.0.10:3000/dishes'),
+      Uri.parse('http://192.168.3.4:3000/dishes'),
     );
     if (response.statusCode == 200) {
       final decodedJson = jsonDecode(response.body);
@@ -124,64 +124,371 @@ class _DishesList extends State<DishesList> {
     );
   }
 
-  //TODO: add delete/sub button and design of dialog
+  //TODO: button del/sub and calling diffrent functions, change ifs into new class that is called with tag
 
   Future<void> _dishDialog(BuildContext context, int index) {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
         return SimpleDialog(
-          title: Text(dishesData[index].name),
+          backgroundColor: const Color.fromARGB(255, 255, 245, 228),
+          title: Text(
+            dishesData[index].name,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.roboto(
+              textStyle: TextStyle(
+                color: const Color.fromARGB(255, 149, 35, 35),
+              ),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           children: <Widget>[
-            Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.all(5),
-                  height: 170,
-                  width: MediaQuery.sizeOf(context).width * 0.8,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(dishesData[index].pictureUrl),
-                      fit: BoxFit.fill,
+            Padding(
+              padding: const EdgeInsets.all(0),
+              child: SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.6,
+                width: MediaQuery.sizeOf(context).width * 0.9,
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(5),
+                      height: 170,
+                      width: MediaQuery.sizeOf(context).width * 0.7,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(dishesData[index].pictureUrl),
+                          fit: BoxFit.fill,
+                        ),
+                        border: Border.all(
+                          width: 3,
+                          color: const Color.fromARGB(255, 255, 245, 228),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: const Color.fromARGB(255, 255, 245, 228),
+                      ),
                     ),
-                    border: Border.all(
-                      width: 3,
-                      color: const Color.fromARGB(255, 255, 245, 228),
+
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 4.0,
+                      children: <Widget>[
+                        if (dishesData[index].porkTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#wieprzowina',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].beefTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#wołowina',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].chickenTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#kurczak',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].eggTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#jajka',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].fishTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#ryba',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].shellfishTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#skorupiaki',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].tofuTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#tofu',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].pastaTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#makaron',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].riceTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#ryż',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].groatsTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#kasza',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].soupTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#zupa',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].saladTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#sałatka',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].vegetablesTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#warzywa',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].chesseTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#ser',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        if (dishesData[index].fruitTag)
+                          Chip(
+                            backgroundColor: const Color.fromARGB(
+                              77,
+                              149,
+                              35,
+                              35,
+                            ),
+                            label: Text(
+                              '#owoce',
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: const Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                      ],
                     ),
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                    color: const Color.fromARGB(255, 255, 245, 228),
-                  ),
-                ),
-                Wrap(
-                  spacing: 8.0,
-                  runSpacing: 4.0,
-                  children: <Widget>[
-                    if (dishesData[index].porkTag)
-                      Chip(label: Text('#wieprzowina')),
-                    if (dishesData[index].beefTag)
-                      Chip(label: Text('#wołowina')),
-                    if (dishesData[index].chickenTag)
-                      Chip(label: Text('#kurczak')),
-                    if (dishesData[index].eggTag) Chip(label: Text('#jajka')),
-                    if (dishesData[index].fishTag) Chip(label: Text('#ryba')),
-                    if (dishesData[index].shellfishTag)
-                      Chip(label: Text('#skorupiaki')),
-                    if (dishesData[index].tofuTag) Chip(label: Text('#tofu')),
-                    if (dishesData[index].pastaTag)
-                      Chip(label: Text('#makaron')),
-                    if (dishesData[index].riceTag) Chip(label: Text('#ryż')),
-                    if (dishesData[index].groatsTag)
-                      Chip(label: Text('#kasza')),
-                    if (dishesData[index].soupTag) Chip(label: Text('#zupa')),
-                    if (dishesData[index].saladTag)
-                      Chip(label: Text('#sałatka')),
-                    if (dishesData[index].vegetablesTag)
-                      Chip(label: Text('#warzywa')),
-                    if (dishesData[index].chesseTag) Chip(label: Text('#ser')),
-                    if (dishesData[index].fruitTag) Chip(label: Text('#owoce')),
+                    Spacer(),
+                    Container(
+                      margin: EdgeInsets.all(5),
+                      height: 50,
+                      width: MediaQuery.sizeOf(context).width * 0.6,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 3,
+                          color: const Color.fromARGB(255, 149, 35, 35),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: const Color.fromARGB(255, 149, 35, 35),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Dodaj lub usuń',
+                          style: GoogleFonts.roboto(
+                            textStyle: TextStyle(
+                              color: Color.fromARGB(255, 255, 245, 228),
+                            ),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           ],
         );
