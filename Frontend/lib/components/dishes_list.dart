9c32e207c67a13b6e563fba489019be604230c85
Jerzy Raszka <jerzyraszka2000@gmail.com>
2025-05-05 13:56:18 +0200

    feat: turned off back button

--- Frontend/lib/components/dishes_list.dart
+++ Frontend/lib/components/dishes_list.dart
@@ -70,100 +70,101 @@ class _DishesList extends State<DishesList> {
   }
 
   bool _isLiked(int index) {
-    if (savedDishesID.contains(dishesData[index].id)) {
-      return true;
-    } else {
-      return false;
-    }
+    return savedDishesID.contains(dishesData[index].id) ? true : false;
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: dishesData.length,
-      itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(
-          onTap: () => _dishDialog(context, index),
-          child: Center(
-            child: Container(
-              margin: EdgeInsets.all(5),
-              width: MediaQuery.sizeOf(context).width * 0.9,
-              height: 230,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 3,
+    return PopScope(
+      canPop: false,
+      child: ListView.builder(
+        itemCount: dishesData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () => _dishDialog(context, index),
+            child: Center(
+              child: Container(
+                margin: EdgeInsets.all(5),
+                width: MediaQuery.sizeOf(context).width * 0.9,
+                height: 230,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 3,
+                    color: Color.fromARGB(255, 149, 35, 35),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
                   color: Color.fromARGB(255, 149, 35, 35),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-                color: Color.fromARGB(255, 149, 35, 35),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(5),
-                    height: 170,
-                    width: MediaQuery.sizeOf(context).width * 0.8,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(dishesData[index].pictureUrl),
-                        fit: BoxFit.fill,
-                      ),
-                      border: Border.all(
-                        width: 3,
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(5),
+                      height: 170,
+                      width: MediaQuery.sizeOf(context).width * 0.8,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(dishesData[index].pictureUrl),
+                          fit: BoxFit.fill,
+                        ),
+                        border: Border.all(
+                          width: 3,
+                          color: const Color.fromARGB(255, 255, 245, 228),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
                         color: const Color.fromARGB(255, 255, 245, 228),
                       ),
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      color: const Color.fromARGB(255, 255, 245, 228),
                     ),
-                  ),
-                  Stack(
-                    children: [
-                      Center(
-                        child: Container(
-                          constraints: BoxConstraints(
-                            maxWidth: MediaQuery.sizeOf(context).width * 0.7,
-                          ),
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              width: 3,
+                    Stack(
+                      children: [
+                        Center(
+                          child: Container(
+                            constraints: BoxConstraints(
+                              maxWidth: MediaQuery.sizeOf(context).width * 0.7,
+                            ),
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                width: 3,
+                                color: const Color.fromARGB(255, 255, 245, 228),
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
                               color: const Color.fromARGB(255, 255, 245, 228),
                             ),
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                            color: const Color.fromARGB(255, 255, 245, 228),
-                          ),
-                          child: Text(
-                            dishesData[index].name,
-                            style: GoogleFonts.roboto(
-                              textStyle: TextStyle(
-                                color: Color.fromARGB(255, 149, 35, 35),
+                            child: Text(
+                              dishesData[index].name,
+                              style: GoogleFonts.roboto(
+                                textStyle: TextStyle(
+                                  color: Color.fromARGB(255, 149, 35, 35),
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
                               ),
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                              overflow: TextOverflow.ellipsis,
                             ),
-                            overflow: TextOverflow.ellipsis,
                           ),
                         ),
-                      ),
-                      if (!_isLiked(index) || widget.deleteOperation)
-                        Container(
-                          alignment: Alignment.centerRight,
-                          child: Icon(
-                            widget.deleteOperation
-                                ? Icons.delete
-                                : Icons.favorite,
-                            size: 35,
-                            color: const Color.fromARGB(255, 255, 245, 228),
+                        if (!_isLiked(index) || widget.deleteOperation)
+                          Container(
+                            alignment: Alignment.centerRight,
+                            child: Icon(
+                              widget.deleteOperation
+                                  ? Icons.delete
+                                  : Icons.favorite,
+                              size: 35,
+                              color: const Color.fromARGB(255, 255, 245, 228),
+                            ),
                           ),
-                        ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 
